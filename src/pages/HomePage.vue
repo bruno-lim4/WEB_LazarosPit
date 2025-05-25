27d83d679828a000b95801c16b9e7460c84f771e
Bruno Lima <brunofigueiredolima@gmail.com>
2025-05-25 09:55:22 -0300

    router adicionado

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+    <NavBar @buscaFoiRealizada="buscaPorNomeDeProdutoNaAPI"/>
+</template>
+
+<script>
+import NavBar from '../components/NavBar.vue';
+
+export default {
+    name: 'HomePage ',
+
+    components: {
+        NavBar,
+    },
+
+    data: () => ({
+        
+    }),
+    methods: {
+        buscaPorNomeDeProdutoNaAPI(pesquisa) {
+        console.log(pesquisa);
+        }
+    }
+    }
+</script>
+  
