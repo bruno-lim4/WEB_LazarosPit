d971a1b9aeed9fdd5339fbf8dfe2ad23cffe3394
Bruno Lima <brunofigueiredolima@gmail.com>
2025-05-26 15:23:57 -0300

    navbar responsiva e funcional

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,149 +1,126 @@
 <template>
   <v-app-bar app color="black" dark :elevation="2">
-    <v-container fluid>
-      <v-row align="center" justify="space-between" no-gutters>
-        <!-- Logo -->
-        <v-col cols="3" class="d-flex align-center">
-          <v-btn @click="goToHomePage">
-            <v-img src="/favicon.ico" max-width="40" max-height="40" />
-            <v-app-bar-title class=" login-title-col " >Lazaro's Pito's Pit</v-app-bar-title>
-          </v-btn>
-        </v-col>
+    <v-row class="align-center pl-3 pr-1" no-gutters>
+      
+      <!-- Logo -->
+      <v-col cols="3" class="d-flex align-center" >
+          <v-img src="/favicon.ico" max-width="40" min-width="30" class="mr-4" style="cursor: pointer;" @click="goToHomePage"/>
+          <v-app-bar-title class="login-title-col" style="white-space: nowrap;">
+            Lazaro's Pit
+          </v-app-bar-title>
+      </v-col>
 
-        <!-- Busca -->
-        <v-col cols="6" class="d-flex justify-center" >
-          <v-text-field
-            v-model="pesquisa"
-            append-inner-icon="mdi-magnify"
-            density="compact"
-            variant="solo"
-            label="Search"
-            hide-details
-            style="max-width: 400px;"
-            bg-color="white"
-          />
-        </v-col>
 
-        <!-- Login e Carrinho (escondido em telas <= 735px) -->
-        <v-col cols="3" class="login-cart-col">
-          <v-btn variant="text" class="text-white" @click="goToLoginPage" v-if="!userIsLoggedIn">
+      <!-- Campo de busca -->
+      <v-col cols="6" class="d-flex justify-center">
+        <v-text-field
+          v-model="pesquisa"
+          append-inner-icon="mdi-magnify"
+          density="compact"
+          variant="solo"
+          label="Search"
+          hide-details
+          style="max-width: 400px;"
+          bg-color="white"
+        />
+      </v-col>
+
+      <!-- Ações (login, admin, logout, carrinho, calculadora) -->
+      <v-col cols="3" class="login-cart-col">
+        <template v-if="!userIsLoggedIn">
+          <v-btn variant="text" class="text-white" @click="goToLoginPage">
             <v-icon class="mr-1">mdi-account</v-icon>
             <div class="d-flex flex-column text-left" style="line-height: 1">
-              <small>Register</small>
-              <strong>Login</strong>
+              <small>Register</small><strong>Login</strong>
             </div>
           </v-btn>
-          <v-btn variant="text" class="text-white" @click="goToAdminPage" v-if="userIsAdmin">
-            <v-icon class="mr-1">mdi-account</v-icon>
-            <div class="d-flex flex-column text-left" style="line-height: 1">
-              <small>Admin</small>
-            </div>
-          </v-btn>
-          <v-btn variant="text" class="text-white mr-5" @click="logUserOut" v-if="userIsLoggedIn">
+        </template>
+        <template v-else>
+          <v-btn variant="text" class="text-white mr-5" @click="logUserOut">
             <v-icon class="mr-1">mdi-exit-run</v-icon>
             <div class="d-flex flex-column text-left" style="line-height: 1">
               <small>Log Out</small>
             </div>
           </v-btn>
-          <v-btn icon @click="goToCalculatorPage">
-            <v-icon>mdi-calculator</v-icon>
-          </v-btn>
-          <v-btn @click="goToCart" icon class="text-white mr-2">
-            <v-icon>mdi-cart</v-icon>
-          </v-btn>
-        </v-col>
+        </template>
+        <v-btn variant="text" class="text-white" @click="goToAdminPage" v-if="userIsAdmin">
+          <v-icon class="mr-1">mdi-account</v-icon>
+          <small>Admin</small>
+        </v-btn>
+        <v-btn icon @click="goToCalculatorPage">
+          <v-icon>mdi-calculator</v-icon>
+        </v-btn>
+        <v-btn icon class="text-white mr-2" @click="goToCart">
+          <v-icon>mdi-cart</v-icon>
+        </v-btn>
+      </v-col>
 
-        <!-- Menu Sanduíche (somente em telas <= 735px) -->
-        <v-app-bar-nav-icon class="menu-icon" @click="drawer = true" />
-      </v-row>
-    </v-container>
+      <v-spacer />
+      <!-- Ícone de menu (mobile) -->
+      <v-app-bar-nav-icon class="menu-icon" @click="drawer = true" />
+    </v-row>
 
+    <!-- Drawer lateral para telas pequenas -->
     
   </v-app-bar>
-  <!-- Drawer lateral -->
-  <v-navigation-drawer
-    v-model="drawer"
-    temporary
-    right
-  >
-    <v-list>
-      <v-list-item v-if="userIsAdmin" @click="goToAdminPage" link>
-        <v-list-item-icon>
+  <v-navigation-drawer v-model="drawer" temporary right>
+      <v-list>
+        <v-list-item v-if="userIsAdmin" @click="goToAdminPage" link>
           <v-icon>mdi-account</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title>Admin</v-list-item-title>
-      </v-list-item>
-      <v-list-item v-if="!userIsLoggedIn" @click="goToLoginPage" link>
-        <v-list-item-icon>
+          <v-list-item-title>Admin</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item v-if="!userIsLoggedIn" @click="goToLoginPage" link>
           <v-icon>mdi-account</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title>Login / Register</v-list-item-title>
-      </v-list-item>
-      <v-list-item v-if="userIsLoggedIn" @click="logUserOut" link>
-        <v-list-item-icon>
+          <v-list-item-title>Login / Register</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item v-if="userIsLoggedIn" @click="logUserOut" link>
           <v-icon>mdi-exit-run</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title>Log Out</v-list-item-title>
-      </v-list-item>
-      <v-list-item @click="goToCart" link>
-        <v-list-item-icon>
+          <v-list-item-title>Log Out</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item @click="goToCart" link>
           <v-icon>mdi-cart</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title>Carrinho</v-list-item-title>
-      </v-list-item>
-      <v-list-item @click="goToCalculatorPage" link>
-        <v-list-item-icon>
+          <v-list-item-title>Carrinho</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item @click="goToCalculatorPage" link>
           <v-icon>mdi-calculator</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title>Macro Calculator</v-list-item-title>
-      </v-list-item>
-    </v-list>
-  </v-navigation-drawer>
+          <v-list-item-title>Macro Calculator</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 </template>
 
 <script>
 export default {
   name: 'NavBar',
+  props: {
+    userIsLoggedIn: Boolean,
+    userIsAdmin: Boolean
+  },
   data() {
     return {
       pesquisa: '',
-      drawer: false,
-    }
-  },
-  props:{
-    userIsLoggedIn:{
-      type: Boolean,
-      default: false,
-    },
-    userIsAdmin:{
-      type: Boolean,
-      default: false,
+      drawer: false
     }
   },
   watch: {
-    pesquisa(newValue) {
-      this.$emit('buscaFoiRealizada', newValue)
-    },
+    pesquisa(newVal) {
+      this.$emit('buscaFoiRealizada', newVal)
+    }
   },
   methods: {
-    goToLoginPage() {
-      this.$router.push({name: 'LoginPage'})
+    goTo(routeName) {
+      this.$router.push({ name: routeName })
     },
-    goToHomePage() {
-      this.$router.push({ name: 'HomePage' })
-    },
-    goToCart() {
-      this.$router.push({ name: 'CartPage' })
-    },
-    goToCalculatorPage() {
-      this.$router.push({ name: 'CalculatorPage' })
-    },
-    goToAdminPage() {
-      this.$router.push({ name: 'AdminDashboard' })
-    },
-    logUserOut() {
-      this.$emit("logUserOut");
-    }
+    goToHomePage() { this.goTo('HomePage') },
+    goToLoginPage() { this.goTo('LoginPage') },
+    goToCart() { this.goTo('CartPage') },
+    goToCalculatorPage() { this.goTo('CalculatorPage') },
+    goToAdminPage() { this.goTo('AdminDashboard') },
+    logUserOut() { this.$emit('logUserOut') }
   }
 }
 </script>
@@ -155,24 +132,6 @@ export default {
   justify-content: flex-end;
 }
 
-.login-title-col {
-  display: flex;
-  align-items: center;
-}
-
-@media (max-width: 500px) {
-  .login-title-col {
-    display: none !important;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    max-width: calc(100% - 40px); /* ajusta conforme o tamanho do ícone */
-    display: flex;
-    align-items: center;
-  }
-}
-
-/* Esconde login/carrinho em telas pequenas */
 @media (max-width: 1100px) {
   .login-cart-col {
     display: none !important;
@@ -182,10 +141,16 @@ export default {
   }
 }
 
-/* Esconde ícone do menu em telas grandes */
 @media (min-width: 1100px) {
   .menu-icon {
     display: none !important;
   }
 }
+
+@media (max-width: 735px) {
+  .login-title-col {
+    display: none !important;
+  }
+}
+
 </style>
